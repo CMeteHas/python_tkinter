459ee006163f32c1330e7b4d621985e4a983d24f
cagan <caganmetehas@gmail.com>
2024-07-22 17:19:36 +0300

    Added new code.

---
+++ Tkinter_Python_Dersleri_9.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+form=tk.Tk()
+form.wm_geometry('500x500+250+250')
+
+form.title("Mete'nin Denemeleri-9")
+buton=tk.Button(text="Deneme",bg="yellow",fg="red",font="times 17 bold")
+buton.place(width=150,height=160)
+ #           (x=50,y=60)
+ #           (relx=550,rely=60)
+
+
+
+
+form.mainloop()
